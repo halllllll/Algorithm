b4221dbd538799c2904c50bed0a8732b87dcb0bd
halllllll <h4l@yahoo.ne.jp>
2019-02-25 13:31:44 +0900

    あーなる 凡例

--- Practice/atcoder/ABC/072/src/_c_whatsthefuck.rb
+++ Practice/atcoder/ABC/072/src/_c_whatsthefuck.rb
@@ -1,4 +1,3 @@
-# 最頻値+-1する。
 n = gets.chomp.to_i
 arr = gets.chomp.split.map(&:to_i).sort
 table = {}
@@ -10,30 +9,20 @@ arr.each do |ai|
   end
 end
 koho = []
-biggest_one = table.values.max
-# ありえるのは最頻値の+-1なので候補に追加しておく
-# ↑どうやらこの前提が間違ってる臭い？?????
-# - なんで間違ってるのかは不明 あってんだろこれ
-table.each do |k, v|
-  if v==biggest_one
-    koho << k-1 << k << k+1
-  end
-end
-koho.uniq!
 # 最頻値の+-1の数とそのtable[+-1]の数を調べる。
+# ↑間違い 2 2 2 4 4 5 5 6 6 7 7とか
+# なので、出てきたやつを試していく
 max_count = 1
 koho.each do |mi|
-  if table.has_key?(mi)
-    # puts "#{mi}の周囲"
-    count = 0
-    [-1, 0, 1].each do |i|
-      if table.has_key?(mi+i)
-        count+=table[mi+i]
-      end
+  # puts "#{mi}の周囲"
+  count = 0
+  [-1, 0, 1].each do |i|
+    if table.has_key?(mi+i)
+      count+=table[mi+i]
     end
-    # puts "#{mi}の周囲の合計 #{count}"
-    max_count = [max_count, count].max
   end
+  # puts "#{mi}の周囲の合計 #{count}"
+  max_count = [max_count, count].max
 end
 
 puts max_count
