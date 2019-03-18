de2b54de90ce4ed7ae6f9e0ee9c668dd91af479e
halllllll <h4l@yahoo.ne.jp>
2019-03-18 10:59:40 +0900

    なんとか

--- Practice/atcoder/ABC/075/src/c.py
+++ Practice/atcoder/ABC/075/src/c.py
@@ -1,44 +1,30 @@
 # よくわからんが隣接リストのDFSで間に合うのでは
 
-# 酔ってるせいのはずだが実装が合わない kuso
-
-# 消したら困るのでとりあえずgit commitしとくわ ザマミロ俺
 n, m = map(int, input().split())
-gets = []
+gets = []  # とっておく
 for _ in range(m):
     s, t = map(int, input().split())
     gets.append([s-1, t-1])
 ans = 0
 for i in range(m):
-    graph = [[] for _ in range(n)]
-    dis_idx = 0
+    graph = [[] for _ in range(n)]  # 隣接リスト
     for j in range(m):
-        if i == j:
-            continue
-        else:
+        # 採用しない橋以外を採用する
+        if i != j:
             s, t = gets[j]
             graph[s].append(t)
             graph[t].append(s)
-
-    # 連結木になるか探索
-    stack = []
-    for g in graph:
-        if len(g) != 0:
-            stack = g
-            break
-    print("start by ", stack)
-    visited = [False for _ in range(len(graph))]
-    while len(stack) > 0:
+    stack = [0]
+    visited = [False for _ in range(n)]
+    # 適当に0から始めて伸ばす
+    visited[0] = True
+    while len(stack):
         nex = stack.pop()
-        for ni in graph[nex]:
-            if visited[ni] == False:
-                visited[ni] = True
-                stack.append(ni)
-    # 判定 すべてのノードを通ったかどうか
+        for g in graph[nex]:
+            if visited[g] == False:
+                visited[g] = True
+                stack.append(g)
     count = list(filter(lambda x: x == True, visited))
-    if len(count) != n:
-        print("fuck in ", i)
-        for g in graph:
-            print(g)
-    ans += 1 if len(count) == n else 0
-print(n-ans)
+    ans += 1 if n-len(count) > 0 else 0
+
+print(ans)
