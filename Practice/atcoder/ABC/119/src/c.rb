0c5aafadaf3cb0f07e2ec22244487f17a07cdfc5
halllllll <h4l@yahoo.ne.jp>
2019-03-31 11:15:06 +0900

    biribiri⚡

---
+++ Practice/atcoder/ABC/119/src/c.rb
@@ -0,0 +1,24 @@
+# 気づかん
+# 手続き的に考えたらまず思いつかない
+# とりあえず全体を俯瞰してみることが大切ってわかる問題
+
+# N本のそれぞれについてA, B, Cのどれかの材料にする或いはどこにも採用しないという4通りをとるとして全探索
+# そうしてできた各長さと実際のA,B,Cとの差を取ることで,インクリメント/デクリメントした際のMPはわかる寸法
+# 合成したときに10使うところの計算をちゃんとやる
+# (↑解説のやつは最初から足しちゃって最後にまとめて引いている)
+
+N, A, B, C = gets.chomp.split.map(&:to_i)
+@kadomatsuz = N.times.map{gets.chomp.to_i}
+
+def rec(i, a, b, c)
+  if i == N
+    return [a, b, c].min > 0 ? (A-a).abs + (B-b).abs + (C-c).abs : 10**10
+  end
+  x = rec(i+1, a, b, c)
+  y = rec(i+1, a+@kadomatsuz[i], b, c) + (a.zero? ? 0 : 10)
+  z = rec(i+1, a, b+@kadomatsuz[i], c) + (b.zero? ? 0 : 10)
+  w = rec(i+1, a, b, c+@kadomatsuz[i]) + (c.zero? ? 0 : 10)
+  return [x, y, z, w].min
+end
+
+puts rec(0, 0, 0, 0)
