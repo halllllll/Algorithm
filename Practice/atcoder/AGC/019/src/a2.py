67c293a9a4994dc7690dc4b72d887c04ace6864d
halllllll <h4l@yahoo.ne.jp>
2019-08-06 22:45:55 +0900

    mu-

--- Practice/atcoder/AGC/019/src/a2.py
+++ Practice/atcoder/AGC/019/src/a2.py
@@ -1,4 +1,11 @@
 # 2Lと1Lを基準にして貪欲
-q, h, s, d = list(reversed(list(map(int, input().split()))))
+q, h, s, d = map(int, input().split())
 n = int(input())
-
+arr = [8 * q, 4 * h, 2 * s, d]
+x = min(arr)
+if n % 2 == 0:
+    print((n // 2) * x)
+else:
+    ans = (n // 2) * x
+    ans += min([4 * q, 2 * h, s])
+    print(ans)
