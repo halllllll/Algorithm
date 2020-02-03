453114c164e75cd82784491a66ed1113a3e455f1
halllllll <h4l@yahoo.ne.jp>
2020-02-03 13:05:54 +0900

    ら

---
+++ Practice/atcoder/ABC/050/src/b.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var out = bufio.NewWriter(os.Stdout)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer out.Flush() // !!!!coution!!!! you must use Fprint(out, ) not Print()
+	/* --- code ---*/
+	n := nextInt()
+	ts := nextInts(n)
+	m := nextInt()
+
+	for i := 0; i < m; i++ {
+		p, x := nextInt()-1, nextInt()
+		ans := 0
+		for j := 0; j < n; j++ {
+			if j != p {
+				ans += ts[j]
+			} else {
+				ans += x
+			}
+		}
+		fmt.Fprint(out, ans)
+	}
+}
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	a, _ := strconv.Atoi(next())
+	return a
+}
+
+func nextFloat64() float64 {
+	a, _ := strconv.ParseFloat(next(), 64)
+	return a
+}
+
+func nextInts(n int) []int {
+	ret := make([]int, n)
+	for i := 0; i < n; i++ {
+		ret[i] = nextInt()
+	}
+	return ret
+}
+func nextFloats(n int) []float64 {
+	ret := make([]float64, n)
+	for i := 0; i < n; i++ {
+		ret[i] = nextFloat64()
+	}
+	return ret
+}
+func nextStrings(n int) []string {
+	ret := make([]string, n)
+	for i := 0; i < n; i++ {
+		ret[i] = next()
+	}
+	return ret
+}
+
+func PrintOut(src interface{}, joinner string) {
+	switch reflect.TypeOf(src).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(src)
+		for idx := 0; idx < s.Len(); idx++ {
+			fmt.Fprintf(out, "%v", s.Index(idx))
+			if idx < s.Len()-1 {
+				fmt.Fprintf(out, "%s", joinner)
+			} else {
+				fmt.Fprintln(out)
+			}
+		}
+	default:
+		fmt.Fprintln(out, "fuck")
+	}
+}
