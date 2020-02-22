fbf2f33abd8f7150c1eb47bae22c4c83ea9fb9dc
halllllll <h4l@yahoo.ne.jp>
2020-02-22 16:40:47 +0900

    寝てた

---
+++ Practice/AOJ/ALDS1/ALDS1_04/ALDS1_04B/alds1_04b.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"reflect"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var out = bufio.NewWriter(os.Stdout)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	defer out.Flush() // !!!!coution!!!! you must use Fprint(out, ) not Print()
+	/* --- code --- */
+	n := nextInt()
+	s := nextInts(n)
+	q := nextInt()
+	t := nextInts(q)
+	// さすがにソートくらいはライブラリ使わせてくれ
+	sort.Ints(s)
+	ans := 0
+	for _, tv := range t {
+		lb, ub := lower_bound(s, tv), upper_bound(s, tv)
+		if lb < ub {
+			ans += 1
+		}
+	}
+	fmt.Fprintln(out, ans)
+}
+
+func lower_bound(arr []int, target int) int {
+	l, r := 0, len(arr)
+	for l < r {
+		mid := (l + r) / 2
+		if arr[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+	return l
+}
+
+func upper_bound(arr []int, target int) int {
+	l, r := 0, len(arr)
+	for l < r {
+		mid := (l + r) / 2
+		if target < arr[mid] {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return l
+}
+
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	a, _ := strconv.Atoi(next())
+	return a
+}
+
+func nextFloat64() float64 {
+	a, _ := strconv.ParseFloat(next(), 64)
+	return a
+}
+
+func nextInts(n int) []int {
+	ret := make([]int, n)
+	for i := 0; i < n; i++ {
+		ret[i] = nextInt()
+	}
+	return ret
+}
+func nextFloats(n int) []float64 {
+	ret := make([]float64, n)
+	for i := 0; i < n; i++ {
+		ret[i] = nextFloat64()
+	}
+	return ret
+}
+func nextStrings(n int) []string {
+	ret := make([]string, n)
+	for i := 0; i < n; i++ {
+		ret[i] = next()
+	}
+	return ret
+}
+
+func PrintOut(src interface{}, joinner string) {
+	switch reflect.TypeOf(src).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(src)
+		for idx := 0; idx < s.Len(); idx++ {
+			fmt.Fprintf(out, "%v", s.Index(idx))
+			if idx < s.Len()-1 {
+				fmt.Fprintf(out, "%s", joinner)
+			} else {
+				fmt.Fprintln(out)
+			}
+		}
+	default:
+		fmt.Fprintln(out, "fuck")
+	}
+}
