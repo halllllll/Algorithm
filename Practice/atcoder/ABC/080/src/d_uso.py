c7c402b5bf8f1d3d2d90fd486a1aed745287e1a0
halllllll <h4l@yahoo.ne.jp>
2020-04-07 18:50:52 +0900

    USO

--- Practice/atcoder/ABC/080/src/d_uso.py
+++ Practice/atcoder/ABC/080/src/d_uso.py
@@ -1,29 +1,32 @@
 # N個のimosなので二次元でやる 0.5がうざいので*2する( (2*10^5) * C )
 
-# なぜか2WAが取れない
+# これ嘘解法ぽい気がする
 
 N, C = map(int, input().split())
-x = 2 * (10 ** 5)
-
+x = 2 * (10**5)
+# x = 30  # test
 imos = [[0 for _ in range(x)] for _ in range(C)]
 for _ in range(N):
     s, t, c = map(int, input().split())
     s, t, c = s * 2 - 1, t * 2 - 1, c - 1
     imos[c][s - 1] += 1
     imos[c][t] -= 1
-
+    # imos[c][s - 1] = min(1, imos[c][s - 1])  # test
 
 for i in range(C):
     for j in range(1, x):
         imos[i][j] += imos[i][j - 1]
+        # imos[i][j] = min(1, imos[i][j])  # test
 
 ans = 0
 for j in range(x):
     tmp = 0
     for i in range(C):
-        tmp += imos[i][j]
+        tmp += min(1, imos[i][j])  # 明らかにUSO
     ans = max(ans, tmp)
-
+# print("-----------")
+# for x in imos:
+#     print(x)
 print(ans)
 
 # 1 3
---
+++ Practice/atcoder/AGC/023/src/a.py
@@ -0,0 +1,18 @@
+# スーパーウルトラ激ムズ200点
+# A[i]...A[j]の累積和をB[j]-B[i-1]で表すと題意よりB[j]-B[i-1] = 0の箇所を数える
+# よってB[j] = B[i-1]となるi,j(i<j)の組の組み合わせの総数となる
+# これは適当に2つ選ぶ組み合わせなのでその数がP個あるならP*(P-1)/2でいける
+n = int(input())
+arr = list(map(int, input().split()))
+d = {0: 1}
+b = [0]
+for i in range(n):
+    b.append(b[i] + arr[i])
+    if b[i + 1] in d:
+        d[b[i + 1]] += 1
+    else:
+        d[b[i + 1]] = 1
+ans = 0
+for _, v in d.items():
+    ans += (v * (v - 1)) // 2
+print(ans)
